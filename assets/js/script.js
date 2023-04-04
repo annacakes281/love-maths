93c89389f1506a3da03a59a99d831f901655f452
annacakes281 <anna.mcconnell@hotmail.co.uk>
2023-04-04 14:40:48 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,10 +30,14 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 /**
@@ -68,7 +72,10 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }
+    else {
         alert(`Unimplemeneted operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -105,8 +112,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 
